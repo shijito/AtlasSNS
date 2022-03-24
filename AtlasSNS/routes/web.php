d1862c676ea4774ee2d404787eb8e2486f68b5a8
shijito <zero.seiyuu@gmail.com>
2022-03-24 19:15:26 +0900

    検索リンク設置

--- AtlasSNS/routes/web.php
+++ AtlasSNS/routes/web.php
@@ -34,10 +34,8 @@ Route::get('/top','PostsController@index');
 
 Route::get('/profile','UsersController@profile');
 
-Route::get('/search','UsersController@index');
+Route::get('/search','UsersController@search');
 
-Route::get('/follow-list','PostsController@index');
-Route::get('/follower-list','PostsController@index');
 
 //ログアウト
 Route::get('/logout','Auth\LoginController@logout');
